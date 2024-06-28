c2497acbba2b66ff6591ffe34f8754791ee61fc4
Silvio <silvionovasmartinez@gmail.com>
2024-06-28 20:47:40 +0200

    asd

--- components/followed.tsx
+++ components/followed.tsx
@@ -6,11 +6,12 @@ import { Button } from "./ui/button";
 export default async function AuthButton() {
   const supabase = createClient();
 
-  const s = await supabase.auth.getSession();
-  const u = await supabase.auth.getUser();
-  console.log(s.data.session?.provider_token);
-  console.log(u.data.user?.user_metadata.provider_id);
-  console.log(u.data.user?.identities?.[0]?.identity_data);
+  const { data, error } = await supabase.auth.getSession();
+  const token = data.session?.provider_token;
+  const userId = data.session?.user.user_metadata;
+  const clientId = "";
+
+  console.log(userId);
 
   return <div>test</div>;
 }

0c74e0f1ea2c4d9304d078baf1fdc5ad7f6ad33c
Silvio <silvionovasmartinez@gmail.com>
2024-06-28 21:28:10 +0200

    asdddddd

--- components/followed.tsx
+++ components/followed.tsx
@@ -1,26 +1,50 @@
+import { useEffect, useState } from "react";
 import { createClient } from "@/utils/supabase/server";
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import { Button } from "./ui/button";
 
-export default async function AuthButton() {
-  const supabase = createClient();
+export default function Followed() {
+  const [streams, setStreams] = useState([]);
 
-  const { data, error } = await supabase.auth.getSession();
-  const token = data.session?.provider_token;
-  const userId = data.session?.user.user_metadata.provider_id;
-  const clientId = process.env.TWITCH_CLIENT_ID;
+  useEffect(() => {
+    const fetchStreams = async () => {
+      const supabase = createClient();
 
-  fetch(`https://api.twitch.tv/helix/streams/followed?user_id=${userId}`, {
-    method: "GET",
-    headers: {
-      "Client-ID": `${clientId}`,
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data))
-    .catch((error) => console.error("Error:", error));
+      const { data } = await supabase.auth.getSession();
 
-  return <div>test</div>;
+      const userId = data.session?.user.user_metadata.provider_id;
+      const token = data.session?.provider_token;
+
+      try {
+        const response = await fetch(
+          `https://api.twitch.tv/helix/streams/followed?user_id=${userId}`,
+          {
+            method: "GET",
+            headers: {
+              "Client-ID": `${process.env.TWITCH_CLIENT_ID}`,
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        if (response.ok) {
+          setStreams(data.data);
+        }
+      } catch (fetchError) {
+        console.error("Error fetching streams:", fetchError);
+      }
+    };
+
+    fetchStreams();
+  }, []);
+
+  return (
+    <div>
+      <h1>Live Streams You Follow</h1>
+      <ul>
+        {streams.map((stream) => (
+          <li key={stream}></li>
+        ))}
+      </ul>
+      <script>console.log({streams})</script>
+    </div>
+  );
 }

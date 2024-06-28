2369bfe02cc4c0c49a682fab3c1c1bc2d712905e
Silvio <silvionovasmartinez@gmail.com>
2024-06-28 20:53:25 +0200

    test

--- components/followed.tsx
+++ components/followed.tsx
@@ -8,10 +8,25 @@ export default async function AuthButton() {
 
   const { data, error } = await supabase.auth.getSession();
   const token = data.session?.provider_token;
-  const userId = data.session?.user.user_metadata;
-  const clientId = "";
+  const userId = data.session?.user.user_metadata.provider_id;
+  const clientId = process.env.TWITCH_CLIENT_ID;
 
   console.log(userId);
+
+  const url = `https://api.twitch.tv/helix/streams/followed?user_id=${userId}`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "Client-ID": clientId,
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error("Error:", error));
 
   return <div>test</div>;
 }

5e5d4a94f5a51c18e7544fb85145a677050513e9
Silvio <silvionovasmartinez@gmail.com>
2024-07-02 21:14:02 +0200

    asas

--- app/m3u/route.ts
+++ app/m3u/route.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { createClient } from "@/utils/supabase/server";
+
 interface Followed {
   followed_at: string;
   broadcaster_id: string;
@@ -7,9 +8,38 @@ interface Followed {
   broadcaster_login: string;
 }
 
+async function fetchM3U8Url(broadcaster_login: string): Promise<string | null> {
+  try {
+    const response = await fetch(
+      `https://twitch-m3u8-api.vercel.app/best?s=${broadcaster_login}`
+    );
+
+    if (response.ok) {
+      const m3u8Url = await response.text();
+      if (!m3u8Url.startsWith("{")) {
+        return m3u8Url;
+      } else {
+        const errorResponse = JSON.parse(m3u8Url);
+        if (errorResponse.error !== "streamer is not online") {
+          console.error(
+            `Error al obtener la URL .m3u8 para ${broadcaster_login}: ${errorResponse.error}`
+          );
+        }
+      }
+    } else {
+      console.error(
+        `Error al obtener la URL .m3u8 para ${broadcaster_login}: ${response.statusText}`
+      );
+    }
+  } catch (error) {
+    console.error(`Error al realizar la solicitud a la API de Twitch: ${error}`);
+  }
+  return null;
+}
+
 export async function GET(request: Request, res: NextApiResponse) {
   const { searchParams } = new URL(request.url);
-  const id = searchParams.get("id"); // 'a', 'b', or 'c'
+  const id = searchParams.get("id");
 
   const supabase = createClient(); // Crear el cliente de Supabase
 
@@ -34,29 +64,10 @@ export async function GET(request: Request, res: NextApiResponse) {
     let m3uContent = "#EXTM3U\n";
 
     for (const channel of followedList) {
-      // Obtener la URL .m3u8 del canal
-      const response = await fetch(
-        `https://twitch-m3u8-api.vercel.app/best?s=${channel.broadcaster_login}`
-      );
-
-      if (response.ok) {
-        const m3u8Url = await response.text();
-        if (!m3u8Url.startsWith("{")) {
-          // Verificar si no es un error JSON
-          m3uContent += `#EXTINF:-1, ${channel.broadcaster_name}\n`;
-          m3uContent += `${m3u8Url}\n`;
-        } else {
-          const errorResponse = JSON.parse(m3u8Url);
-          if (errorResponse.error !== "streamer is not online") {
-            console.error(
-              `Error al obtener la URL .m3u8 para ${channel.broadcaster_name}: ${errorResponse.error}`
-            );
-          }
-        }
-      } else {
-        console.error(
-          `Error al obtener la URL .m3u8 para ${channel.broadcaster_name}: ${response.statusText}`
-        );
+      const m3u8Url = await fetchM3U8Url(channel.broadcaster_login);
+      if (m3u8Url) {
+        m3uContent += `#EXTINF:-1, ${channel.broadcaster_name}\n`;
+        m3uContent += `${m3u8Url}\n`;
       }
     }
 

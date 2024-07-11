f916fbe0fb623394b3d5a1e5de4e46fbce4b4695
Silvio <silvionovasmartinez@gmail.com>
2024-07-11 07:31:46 +0200

    asasas

--- components/PlaylistUrl.tsx
+++ components/PlaylistUrl.tsx
@@ -6,7 +6,7 @@ type PlaylistUrl = {
 export default function PlaylistUrl({ id = "" }: PlaylistUrl) {
   let url = `${window.location.href}m3u?id=${id}`;
   return (
-    <div>
+    <div className="flex justify-center mb-4">
       <a className="hover:text-blue-400" href={url}>
         {url}
       </a>

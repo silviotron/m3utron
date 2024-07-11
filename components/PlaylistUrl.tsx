0512338b63918d4e2903e0351546116184f6d6e3
Silvio <silvionovasmartinez@gmail.com>
2024-07-11 07:41:08 +0200

    asi?

--- components/PlaylistUrl.tsx
+++ components/PlaylistUrl.tsx
@@ -1,11 +1,14 @@
-"use client";
-
 type PlaylistUrl = {
   id?: String;
 };
 
 export default function PlaylistUrl({ id = "" }: PlaylistUrl) {
-  let url = `${window.location.href}m3u?id=${id}`;
+  const defaultUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+
+  const url = `${window.location.href}/m3u?id=${id}`;
+
   return (
     <div className="flex justify-center mb-4">
       <a className="hover:text-blue-400" href={url}>

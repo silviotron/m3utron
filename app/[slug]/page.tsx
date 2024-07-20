ca12a86ad25c7a5a03bd82b55639a013f3d6c9b1
Silvio <silvionovasmartinez@gmail.com>
2024-07-20 19:01:09 +0200

    uffline streams ok

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -49,9 +49,26 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
   user = user.data[0];
   stream = stream.data[0];
+  let channel = null;
 
   console.log(user);
   console.log(stream);
+  if (!stream) {
+    const channels = await fetch(
+      `https://api.twitch.tv/helix/channels?broadcaster_id=${user.id}`,
+      {
+        method: "GET",
+        headers: {
+          "Client-ID": `${clientId}`,
+          Authorization: `Bearer ${twitchToken}`,
+        },
+      }
+    );
+
+    channel = await channels.json();
+    channel = channel.data[0];
+    console.log(channel);
+  }
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -71,12 +88,25 @@ export default async function Page({ params }: { params: { slug: string } }) {
 
       <main className="w-full max-h-full">
         <div className="w-[88%] float-right  mt-16 pr-[340px]">
-          <HlsPlayer src={`https://twitch-m3u8-api.vercel.app/p?s=${params.slug}`} />
+          {stream ? (
+            <HlsPlayer src={`https://twitch-m3u8-api.vercel.app/p?s=${params.slug}`} />
+          ) : (
+            <div className="relative">
+              {user.offline_image_url != "" ? (
+                <img src={user.offline_image_url} alt="s" className="w-full h-auto" />
+              ) : (
+                <div className="w-full bg-black aspect-video" />
+              )}
+              <span className="absolute top-4 right-6 bg-black bg-opacity-75 text-white px-1 py-0 text-sm">
+                OFFLINE
+              </span>
+            </div>
+          )}
           <div className=" flex gap-4 mt-2">
             <img
               src={user.profile_image_url}
               alt=""
-              className="rounded-full size-20 my-auto   border-4 border-[#9146FF] p-1"
+              className="rounded-full size-20 my-auto   border-4 border-[#9146FF] p-0"
             />
             <div className="w-full">
               <div className="flex justify-between">
@@ -92,50 +122,58 @@ export default async function Page({ params }: { params: { slug: string } }) {
                 </div>
               </div>
               <div className="flex">
-                <h2>{stream.title}</h2>
-                <div className="ml-auto">
-                  <span className="mr-2">
-                    <span className="animate-pulse">🔴</span>
-                    {stream.viewer_count}
-                  </span>
-                  <span>
-                    {dayjs().diff(dayjs(stream.started_at), "hours")}h{" "}
-                    {dayjs().diff(dayjs(stream.started_at), "minutes") % 60}m
-                  </span>
-                </div>
+                <h2>{stream?.title || channel?.title}</h2>
+                {stream && (
+                  <div className="ml-auto text-nowrap">
+                    <span className="mr-2 ">
+                      <span className="animate-pulse">🔴</span>
+                      {stream.viewer_count}
+                    </span>
+                    <span>
+                      {dayjs().diff(dayjs(stream.started_at), "hours")}h{" "}
+                      {dayjs().diff(dayjs(stream.started_at), "minutes") % 60}m
+                    </span>
+                  </div>
+                )}
               </div>
               <div className="flex ">
-                <a href={`/directory/all/tags/${stream.game_name.replaceAll(" ", "-")}`}>
+                <a
+                  href={`/directory/all/tags/${
+                    stream ? stream.game_name.replaceAll(" ", "-") : channel.game_name
+                  }`}
+                >
                   <span className="text-[#9146FF] whitespace-nowrap hover:underline">
-                    {stream.game_name}
+                    {stream?.game_name || channel.game_name}
                   </span>
                 </a>
-                <Carousel
-                  opts={{
-                    loop: true,
-                    dragFree: false,
-                    skipSnaps: true,
-                    active: false,
-                  }}
-                  className="ml-4"
-                >
-                  <CarouselContent className="select-none ml-0">
-                    {stream.tags.map((tag: string, index: number) => (
-                      <a
-                        href={`/directory/category/${tag}`}
-                        key={index}
-                        className="select-none"
-                      >
-                        <CarouselItem
+                {stream && (
+                  <Carousel
+                    opts={{
+                      loop: true,
+                      dragFree: false,
+                      skipSnaps: true,
+                      active: false,
+                    }}
+                    className="ml-4"
+                  >
+                    <CarouselContent className="select-none ml-0">
+                      {stream.tags.map((tag: string, index: number) => (
+                        <a
+                          href={`/directory/category/${tag}`}
                           key={index}
-                          className="basis-auto  bg-[#53535f] text-[#adadb8] px-2 py-[2px] text-xs font-semibold rounded-full mx-1  hover:opacity-80 max-w-full overflow-hidden"
+                          className="select-none"
                         >
-                          {tag}
-                        </CarouselItem>
-                      </a>
-                    ))}
-                  </CarouselContent>
-                </Carousel>
+                          <CarouselItem
+                            key={index}
+                            className="basis-auto  bg-[#53535f] text-[#adadb8] px-2 py-[2px] text-xs font-semibold rounded-full mx-1  hover:opacity-80 max-w-full overflow-hidden"
+                          >
+                            {tag}
+                          </CarouselItem>
+                        </a>
+                      ))}
+                    </CarouselContent>
+                  </Carousel>
+                )}
               </div>
             </div>
           </div>
